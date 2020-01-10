445b9fe225e8277126406762f289daec6acd2f1b
Austin Czyzewski <40173565+Austin-Czyzewski@users.noreply.github.com>
2020-01-10 14:56:19 -0500

    Update Universal_Plotter.py

--- Dipole Scan/Universal_Plotter.py
+++ Dipole Scan/Universal_Plotter.py
@@ -7,10 +7,11 @@ from IPython.display import display, clear_output
 import glob
 import os
 
+
 Files = glob.glob("*EC.txt")
 Open_Files = []
 for file in range(len(Files)):
-    Temp_file = open(Files[file],'r') 
+    Temp_file = open(Files[file],'r')
     for i in Temp_file:
         Open_Files.append(i.strip("())\n"))
     Temp_file.close()
@@ -21,7 +22,7 @@ Open_Files = np.array([np.array(i).astype(float) for i in Open_Files])
 Emissions = []
 for i in Files:
     Emissions.append(int(i[-10:-6].strip("_")))
-print(Emissions)
+#print(Emissions)
 
 
 Iterator = int(len(Open_Files)/len(Files))
@@ -31,22 +32,28 @@ for i in range(len(Files)):
     All_Emissions[i] = Open_Files[Iterator*i:Iterator*(i+1)]
     
     
+All_Emissions_Avg = np.zeros([len(Files),int([i-1 if i%2 != 0 else i for i in [Iterator]][0]/2),2])
+
+for num,k in enumerate(All_Emissions):
+    for i in range(int(len(k)/2)):
+        for j in range(int(len(k)/2)):
+            #if np.shape(All_Emissions)[1] %2 != 0:
+            if k[i,0] == k[j,0]:
+                All_Emissions_Avg[num,i] = (([k[i,0],(k[i,1]+k[j,1])/2]))
+                
+
 plt.figure(figsize = (11,8.5))
-view_list = range(len(All_Emissions))
-
-
-#view_list = [0,1,20]
-
+view_list = range(len(All_Emissions_Avg))
 
 for run in view_list:
-    #plt.scatter(All_Emissions[run,:,0],100*abs(All_Emissions[run,:,1]/max(abs(All_Emissions[run,:,1]))),label = "{} \u03BCA Emitted".format(Emissions[run]))
-    plt.plot(All_Emissions[run,:,0],100*1000*abs(All_Emissions[run,:,1]/Emissions[run]),label = "{} \u03BCA Emitted".format(Emissions[run]),alpha = 0.5)
+    plt.plot(All_Emissions_Avg[run,:,0],1e5*abs(All_Emissions_Avg[run,:,1]/Emissions[run]),label = "{} \u03BCA Emitted".format(Emissions[run]),alpha = 0.5)
 plt.minorticks_on()
 plt.grid(True,alpha = 0.25,which = 'both',color = 'gray')
 plt.legend()
 plt.title("Collected Current Through DBA Aperture at Various Emissions")
+#plt.xlim(.475,.480)
+#plt.ylim(-5,10)
 plt.xlabel("Current in Dipole 1 (A)")
 plt.ylabel("Current Collected as percentage of Emitted Current")
 plt.gca().invert_xaxis()
-#plt.savefig("DP1_Results.svg",transparent = True)
-
+plt.savefig("DP1_Results.svg",transparent = True)
