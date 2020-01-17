e8da8d18e73966781ad467330ac70f31b23adc68
Austin Czyzewski <40173565+Austin-Czyzewski@users.noreply.github.com>
2020-01-17 15:28:31 -0500

    Updated Dipole Walk code

--- Dipole Scan/DP1_Test_West_V_2_2.py
+++ Dipole Scan/DP1_Test_West_V_2_2.py
@@ -1,7 +1,3 @@
-from pymodbus.client.sync import ModbusTcpClient
-from pymodbus.payload import BinaryPayloadDecoder
-from pymodbus.payload import BinaryPayloadBuilder
-from pymodbus.constants import Endian
 import matplotlib.pyplot as plt
 from datetime import datetime
 import time
@@ -16,57 +12,56 @@ End_Value = float(input("What is the ending amperage that you want to ramp the m
 #Uncomment to make variable number of runs
 #Runs = int(input("How many runs do you want the Dipole to make?   "))
 
-Runs = 1
-Dipole_Tag = 22201
-Step_size = .001
-Read = "11109"
-count = 20
+Runs = 1 #Number of times you want to ramp to the input value and back to the start
+Dipole_Tag = 22201 #Modbus address of the magnet we are writing to
+Step_size = .001 #Step Size, in Amps, that we are taking to reach our goal
+Read = "11109" #Modbus address of the value we want to read while we scan the magnet
+count = 20 #Number of times we want to average the Read Tag value
 
-Start_Value = M.Read(Client, Dipole_Tag)
+Start_Value = M.Read(Client, Dipole_Tag) #Recording the starting value of the Dipole
 print("Started at {0:.3f} Amps".format(Start_Value))
 
 DP1_Values = []
 DBA_Collection = []
 colors = []
+
 for i in range(Runs):
     
     DP1_Vals, DBA_Col = M.Ramp_One_Way(Client, Dipole_Tag, End_Value, Max_Step = Step_size, Return = "Y", Read_Tag = Read, count = count)
-
-    DP1_Values += DP1_Vals
-    DBA_Collection += DBA_Col
-
-    temp_list = ['b' for i in list(range(len(DP1_Vals)))]
-
-    colors += temp_list
+    #The above function walks the magnet to the endpoint ,and returns the data
+    
+    DP1_Values += DP1_Vals #Adding the recorded data to the lists
+    DBA_Collection += DBA_Col 
+    
+    colors += ['chocolate' for i in list(range(len(DP1_Vals)))] #Appending 'chocolate' as the color for this data set
 
     DP1_Vals, DBA_Col = M.Ramp_One_Way(Client, Dipole_Tag, Start_Value, Max_Step = Step_size, Return = "Y", Read_Tag = Read, count = count)
-
+    #The above statement walks us back to the start, and returns the data
+    
     DP1_Values += DP1_Vals
     DBA_Collection += DBA_Col
 
-    temp_list = ['r' for i in list(range(len(DP1_Vals)))]
-
-    colors += temp_list
+    colors += ['firebrick' for i in list(range(len(DP1_Vals)))] #Appending 'firebrick' as the color for this data set
     
     
 
-now = datetime.today().strftime('%y%m%d_%H%M')
+now = datetime.today().strftime('%y%m%d_%H%M') #Grabbing the time and date in a common format to save the plot and txt file to
 plt.figure(figsize = (12,8))
-plt.scatter(DP1_Values,DBA_Collection,color = colors)
+plt.scatter(DP1_Values,DBA_Collection,color = colors, alpha = 0.5)
+
+plt.grid(True,alpha = 0.25,which = 'both',color = 'gray') #Making a grid
+plt.minorticks_on() #Setting the minor ticks
 
 #Naming
-plt.ylabel("DBA current collected")
-plt.xlabel("Magnet Amperage")
+plt.ylabel("DBA current collected (mA)")
+plt.xlabel("Magnet Setting (A)")
 plt.title("Dipole 1 current collected over walk from {0:.3f} to {1:.3f}".format(Start_Value, End_Value))
-plt.suptitle("Blue = Ascending, Red = Descending",fontsize = 8)
+plt.suptitle("Orange = Ascending, Red = Descending",fontsize = 8, alpha = 0.65)
 
 plt.grid(True)
 plt.savefig(now + '_graph.png',trasnparent = True)
 
-Clean_DP1 = [round(i,3) for i in DP1_Values]
-Clean_DBA = [round(i,4) for i in DBA_Collection]
-
-save_list = M.merge(Clean_DP1,Clean_DBA)
+save_list = M.merge(DP1_Values,DBA_Collection)
 with open(now+'.txt', 'w') as f:
     for item in save_list:
         f.write(str(item)+'\n')
