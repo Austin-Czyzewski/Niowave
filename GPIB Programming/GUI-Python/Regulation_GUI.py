7a5b2a94c6112b8aaf66450ac9e0c1bd21d976fb
Austin Czyzewski <40173565+Austin-Czyzewski@users.noreply.github.com>
2020-03-17 15:59:22 -0400

    Updated Style of buttons and colors

--- GPIB Programming/GUI-Python/Regulation_GUI.py
+++ GPIB Programming/GUI-Python/Regulation_GUI.py
@@ -1,47 +1,47 @@
-import GPIB_FUNCS as GPIB #Importing our GPIB communication functions for easier comprehension and use
-import pyvisa #import GPIB communication module
-import time #imports time to sleep program temporarily
-from tkinter import * #importing GUI library
+import GPIB_FUNCS as GPIB
+import pyvisa
+import time
+from tkinter import *
 
+RM = pyvisa.ResourceManager()
+print(RM.list_resources())
+SG = RM.open_resource('GPIB0::10::INSTR')
+OS = RM.open_resource('GPIB0::16::INSTR')
 
-RM = pyvisa.ResourceManager() #pyVISA device manager
-print(RM.list_resources()) #Printing out all detected device IDs
-SG = RM.open_resource('GPIB0::10::INSTR') #Opening the Signal generator as an object
-OS = RM.open_resource('GPIB0::16::INSTR') #Opening the oscilloscope as an object
+running = False # Global flag
+reset = False # Global flag
+pulsing = False # Global flag
 
-running = False # Global flag, used for run status of GUI
-reset = False # Global flag, used to pause modulation and reset the oscilloscope
-pulsing = False # Global flag, used to change the modulation parameters for CW to pulsing
+IF_Channel = 2
+Trigger_Channel = 4
+Trigger_Level = 20   /1000 #mv
 
-IF_Channel = 2 #The channel that the error signal is on
-Trigger_Channel = 4 #The channel which shows the SRF pulse
-Trigger_Level = 20   /1000 #mv #The level of the pulse trigger
+Measurement = 3
+Step_size = 40 #(Hz)
+Pulse_Step_Size = 10 #(Hz)
+Max_Threshold = 10000 #(Hz)
+Walk_Threshold = 2.5 #(mV)
+Pulse_Walk_Threshold = 0.5 #(mV)
+Wait_after_step = 0.0400 #Seconds
+Wait_between_reads = 0.0100 #Seconds
+Long = False
 
-Measurement = 3 #Measurement channel
-Step_size = 40 #(Hz) Change in frequency with each regulation step
-Pulse_Step_Size = 10 #(Hz) Change in frequency with each regulation step when pulsing
-Max_Threshold = 10000 #(Hz) Total amount of frequency change before automatically tripping off program
-Walk_Threshold = 2.5 #(mV) Deviation from 0 the error signal needs to be before CW regulation kicks in
-Pulse_Walk_Threshold = 0.5 #(mV) Deviation from 0 the error signal needs before pulsing regulation kicks in
-Wait_after_step = 0.0400 #Seconds, the time waited after a step is taken, necessary to allow oscope measurements to change
-Wait_between_reads = 0.0100 #Seconds, currently not used, supplemented by GUI time between reads
-Long = False #The form that our measurement is output from the o-scope, depending on the way it is set up this can be in either a short or long form
+GPIB.measurement_setup(OS,IF_Channel, measurement = Measurement)
+GPIB.channel_settings_check(OS, IF_Channel)
+GPIB.trigger_settings_set(OS, Trigger_Channel, Trigger_Level)
+GPIB.vertical_marker_pulsing(OS, IF_Channel)
 
-GPIB.measurement_setup(OS,IF_Channel, measurement = Measurement) #Setting up the required measurement
-GPIB.channel_settings_check(OS, IF_Channel) #Setting up the vertical and horizontal settings for the error signal
-GPIB.trigger_settings_set(OS, Trigger_Channel, Trigger_Level) #Sets up the vertical settings for trigger channel and trigger parameters
-GPIB.vertical_marker_pulsing(OS, IF_Channel) #Sets up vertical cursor bars to read edge of pulse
+interlock_color = 'Yellow'
 
-interlock_color = 'MediumPurple1'
-global Ups #Defining global parameters, these ones are not actually necessary but leave them be
+global Ups
 global Downs
 global i
-Ups = 0 #number of steps taken up before taking one down
-Downs = 0 #visa versa
-i = 0 #total number of iterative loops gone through, only present to show differences in command line readouts
-Start_Freq = GPIB.freq(SG) #taking the start frequency of the signal generator
+Ups = 0
+Downs = 0
+i = 0
+Start_Freq = GPIB.freq(SG)
 
-try: #Test to see if the short form of the oscilloscope or long form of output
+try:
     short_test = float(OS.query("MEASU:MEAS1:VAL?"))
     pass
 except:
@@ -49,74 +49,55 @@ except:
     Long = True
     pass
 
-#print statement
+
 print("\n\n\n")
 print("-" * 60)
 print("Beginning modulation")
 print("-" * 60)
 print("\n\n\n")
 
-def scanning(): #Defining what is happening when scanning our GUI
+def scanning():
     
-    global reset #Global variables, these first three are necessary but again, leave them all.
+    global reset
     global pulsing
     global running
     global Ups
     global Downs
     global i
-    
-    #########################################
-    # Reset button loop
-    #########################################
-    
-    if reset: #Checks the reset parameter and runs if True
-        print("-"*60, "\n\n\nResetting Oscilloscope\n\n\n", "-"*60) #print statement
+    if reset:
+        print("-"*60, "\n\n\nResetting Oscilloscope\n\n\n", "-"*60)
         
-        GPIB.measurement_setup(OS,IF_Channel, measurement = Measurement) #Same as beginning parameters above
+        GPIB.measurement_setup(OS,IF_Channel, measurement = Measurement)
         GPIB.channel_settings_check(OS, IF_Channel)
         GPIB.trigger_settings_set(OS, Trigger_Channel, Trigger_Level)
         GPIB.vertical_marker_pulsing(OS, IF_Channel)
        
-        reset = False #Sets the reset parameter to false so not looping
+        reset = False
+    if running:  # Only do this if the Stop button has not been clicked
+        root.configure(bg = 'SpringGreen2')
         
-    #########################################
-    # Checking for the regulation loop if on
-    #########################################
-
-    if running:  # running parameter, if True, runs this loop
-        root.configure(bg = 'SpringGreen2') #Sets the background of the GUI to be green
-        
-        #########################################
-        # Loop for pulsing operation
-        #########################################
-        
-        if pulsing: #Checks pulsing tag and runs this loop if true
+        if pulsing:
             
-            read_value = GPIB.cursor_vbar_read_mv(OS) #Takes the current value of oscilloscope vbar
+            read_value = GPIB.cursor_vbar_read_mv(OS)
         
-            if read_value > Pulse_Walk_Threshold: #Checks to see if that value is outside of threshold
+            if read_value > Pulse_Walk_Threshold:
                 Ups += 1
                 Downs = 0
-                if Ups > 3: #Effective debounce
-                    temp_freq = GPIB.freq(SG) #Gathers the current frequency
-                    if (temp_freq + Pulse_Step_Size) > (Start_Freq + Max_Threshold): #Sees if the new frequency is outside of bounds
+                if Ups > 3:
+                    temp_freq = GPIB.freq(SG)
+                    if (temp_freq + Pulse_Step_Size) > (Start_Freq + Max_Threshold):
                         print("Broken on too many steps upward")
-                        root.configure(bg = interlock_color) #Sets to the interlock color
-                        running = False #Breaks loop if true
-                    if OS.query("MEASU:Meas{}:State?".format(Measurement))[-2] != str(1): #Sees if the measurement is still active
+                        root.configure(bg = interlock_color)
+                        running = False
+                    if OS.query("MEASU:Meas{}:State?".format(Measurement))[-2] != str(1):
                         print("Measurement Off")
-                        root.configure(bg = interlock_color) #Sets to the interlock color
-                        running = False #Breaks loop if true
-                    GPIB.write_frequency(SG, (temp_freq + Pulse_Step_Size),"HZ") #Writes calculated frequency to the signal generator
-                    print("Raised Frequency ", i) #Shows that we took a step in frequency
-                    time.sleep(Wait_after_step) #Sleep for after step debounce time
-            
-            #########################################
-            # Repeat above loop but below the threshold instead of above
-            #########################################
-            
-            
-            if read_value < -Pulse_Walk_Threshold: 
+                        root.configure(bg = interlock_color)
+                        running = False
+                    GPIB.write_frequency(SG, (temp_freq + Pulse_Step_Size),"HZ")
+                    print("Raised Frequency ", i)
+                    time.sleep(Wait_after_step)
+
+            if read_value < -Pulse_Walk_Threshold:
                 Downs += 1
                 Ups = 0
                 if Downs > 3:
@@ -136,10 +117,6 @@ def scanning(): #Defining what is happening when scanning our GUI
             time.sleep(Wait_between_reads)
             i += 1
             
-        #########################################
-        # Loop for CW operation, use same logic
-        #########################################
-            
         else:
             read_value = GPIB.read_mv(OS, long = Long, measurement = Measurement)
         
@@ -150,7 +127,7 @@ def scanning(): #Defining what is happening when scanning our GUI
                     temp_freq = GPIB.freq(SG)
                     if (temp_freq + Step_size) > (Start_Freq + Max_Threshold):
                         print("Broken on too many steps upward")
-                        root.configure(bg = interlock_color)
+                        #global running
                         running = False
                     if OS.query("MEASU:Meas{}:State?".format(Measurement))[-2] != str(1):
                         print("Measurement Off")
@@ -159,10 +136,6 @@ def scanning(): #Defining what is happening when scanning our GUI
                     GPIB.write_frequency(SG, (temp_freq + Step_size),"HZ")
                     print("Raised Frequency ", i)
                     time.sleep(Wait_after_step)
-                    
-            #########################################
-            # Repeat above loop but below the threshold instead of above
-            #########################################
 
             if read_value < -Walk_Threshold:
                 Downs += 1
@@ -171,7 +144,7 @@ def scanning(): #Defining what is happening when scanning our GUI
                     temp_freq = GPIB.freq(SG)
                     if (temp_freq - Step_size) < (Start_Freq - Max_Threshold):
                         print("Broken on too many steps downward")
-                        root.configure(bg = interlock_color)
+                        #global running
                         running = False
                     if OS.query("MEASU:Meas{}:State?".format(Measurement))[-2] != str(1):
                         print("Measurement Off")
@@ -182,10 +155,10 @@ def scanning(): #Defining what is happening when scanning our GUI
                     time.sleep(Wait_after_step)
 
             #time.sleep(Wait_between_reads)
-            i += 1 #Update iterator
+            i += 1
 
     # After .1 seconds, call scanning again (create a recursive loop)
-    root.after(100, scanning) #Wait the first number of ms, then continue scanning
+    root.after(100, scanning)
 
 
 def start():
@@ -200,34 +173,32 @@ def stop():
     root.configure(bg = 'firebrick1')
 
 def reset_measurement():
-    """Temporarily stops the scan to reset the oscilloscope settings to default used in regulation"""
     root.configure(bg = 'sky blue')
     global reset
     reset = True
     
 def pulsing_toggle():
-    """Toggles the pulsing parameter within the global running flag, detects if true or not currently"""
     global pulsing
-    if pulsing: #If pulsing is true, toggle to false, if false, toggle to true, done so there is no need for two buttons with pulsing
+    if pulsing:
         pulsing = False
     else:
         pulsing = True
     
 
 
-root = Tk() #Beginning the GUI object
-root.title("Regulation Window") #Title of the GUI
-root.geometry("1000x1000") #Size of the GUI
-#root.attributes('-fullscreen', True) #Swap the comments on this line and the previous to determine if the GUI is fullscreen or resizable
+root = Tk()
+root.title("Regulation Window")
+root.geometry("1000x1000")
+#root.attributes('-fullscreen', True)
 
-app = Frame(root) #Application window
-app.grid() #Making this visible
-size = 10 #Size marker used universally between buttons
-start = Button(app, text="Start Regulation", command=start, activebackground="white", height = size, width = size*5, bg = 'SpringGreen2', font=('Helvetica', '20')) #Start button configuration, same for next three lines 
-stop = Button(app, text="Stop Regulation", command=stop, activebackground="white", height = size, width = size*5, bg = 'firebrick1', font=('Helvetica', '20'))
-reset_button = Button(app, text="Reset Oscilloscope", command=reset_measurement, activebackground="white", height = int(size/3), width = size*5, bg = 'sky blue', font=('Helvetica', '20'))
-Pulsing_button = Checkbutton(app, text="Pulsing", command=pulsing_toggle, activebackground="white", height = int(size/3), width = size*4, bg = 'orange', font=('Helvetica', '20'))
-start.grid() #put the start button on the GUI, same for next three lines
+app = Frame(root)
+app.grid()
+size = 10
+start = Button(app, text="Start Regulation", command=start, activebackground="SpringGreen2", height = size, width = size*5, bg = 'Pale Green', font=('Helvetica', '20'), bd = size)
+stop = Button(app, text="Stop Regulation", command=stop, activebackground="firebrick1", height = size, width = size*5, bg = 'tomato', font=('Helvetica', '20'), bd = size)
+reset_button = Button(app, text="Reset Oscilloscope", command=reset_measurement, activebackground="light sky blue", height = int(size/3), width = size*5, bg = 'sky blue', font=('Helvetica', '20'), bd = size)
+Pulsing_button = Checkbutton(app, text="Pulsing", command=pulsing_toggle, activebackground="white", height = int(size/3), width = size*5, bg = 'gray', font=('Helvetica', '20'), bd = size,indicatoron=False,selectcolor='orange')
+start.grid()
 stop.grid()
 reset_button.grid()
 Pulsing_button.grid()
