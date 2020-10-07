cf19ce68b6ff534dafb8511cfd918322e8b0c3ae
Austin Czyzewski <40173565+Austin-Czyzewski@users.noreply.github.com>
2020-10-07 11:50:05 -0400

    Documenting and commenting

--- Python-PLC/Cutoffs/Cutoff_Tester.py
+++ Python-PLC/Cutoffs/Cutoff_Tester.py
@@ -1,3 +1,20 @@
+'''
+Cutoff_Tester.py
+Author: Austin Czyzewski
+Org date: 10/06/2020
+
+Purpose: This script is meant to automate taking the V0 cutoffs 1 and 2
+    using the method proposed by Andrew Schnepp and Sam Baurac. 
+    In this method the gating field is fluctuated linearly with the 
+    bias harmonic field both on and off to help compensate for change in
+    position of the cathode. This data is used to determine the strength 
+    of our accelerating fields and to help determine running parameters.
+    
+Example of how to use it:
+    - Run it
+'''
+
+
 import Master as M
 import Tag_Database as Tags
 import numpy as np
@@ -29,6 +46,8 @@ V0_4 = float(input("V0 Setpoint 4  "))
 ### List of tags to read at every step #########################
 ################################################################
 
+#(Sorry for the stupid double list format. I wrote Gather() before I knew how often I'd use it)
+
 Tag_List = [[Tags.CU_Pf, False], [Tags.CU_Pr, False], [Tags.CU_Pt, False], [Tags.CU_V, False], \
             [Tags.BH_Pf, False], [Tags.BH_Pr, False], [Tags.BH_Pt, False], \
             [Tags.HV_Bias, False], [Tags.V0_SP, False], [Tags.V0_Read, False], \
@@ -44,24 +63,34 @@ Full_Data_Set = list()
 V0_List_1 = np.arange(V0_1, V0_2 + V0_resolution, V0_resolution)
 V0_List_2 = np.arange(V0_3, V0_4 + V0_resolution, V0_resolution)
 
-sleep_time = 0 #Don't sleep on the first pass through
+sleep_time = 0 #Don't take plateau data first run through
 
 for index in range(Num_sawtooths_1): #First loop with V0_1 and V0_2
     
-    time.sleep(sleep_time)
-    
+    #Taking the plateau data
+    runtime = time.time()
+    while time.time() - runtime < sleep_time:
+        Full_Data_Set.append(M.Gather(Client, Tag_List))
+        
+    #Start setting the sleep time if on the first pass in this loop
     if not index:
         start_time = time.time()
         
+    #Ramp one way in V0 while collecting data
     for value in V0_List_1:
         M.Write(Client, Tags.V0_SP, value)
         Full_Data_Set.append(M.Gather(Client, Tag_List))
     
+    #Finish setting the sleep time to what it took to ramp one way
     if not index:
         sleep_time = time.time() - start_time
-        
-    time.sleep(sleep_time)
     
+    #Taking the trough data
+    runtime = time.time()
+    while time.time() - runtime < sleep_time:
+        Full_Data_Set.append(M.Gather(Client, Tag_List))
+    
+    #Reverse! Reverse!
     for value in V0_List_1[::-1]:
         M.Write(Client, Tags.V0_SP, value)
         Full_Data_Set.append(M.Gather(Client, Tag_List))
@@ -74,20 +103,30 @@ M.Write(Client, Tags.BH_VVA_SP, 0)
 
 for index in range(Num_sawtooths_2):
     
-    #Maybe maybe maybe maybe
+    #Plateau (on first run it will match time of first loop)
+    runtime = time.time()
+    while time.time() - runtime < sleep_time:
+        Full_Data_Set.append(M.Gather(Client, Tag_List))
+        
+    #Begin setting new sleep time
     if not index:
         start_time = time.time()
         
+    #Ramp it
     for value in V0_List_2:
         M.Write(Client, Tags.V0_SP, value)
         Full_Data_Set.append(M.Gather(Client, Tag_List))
     
-    #Maybe maybe maybe maybe
+    #Sleep time pt.II
     if not index:
         sleep_time = time.time() - start_time
         
-    time.sleep(sleep_time)
+    #Trough
+    runtime = time.time()
+    while time.time() - runtime < sleep_time:
+        Full_Data_Set.append(M.Gather(Client, Tag_List))
     
+    #Bring it back now, y'all
     for value in V0_List_2[::-1]:
         M.Write(Client, Tags.V0_SP, value)
         Full_Data_Set.append(M.Gather(Client, Tag_List))
@@ -99,7 +138,7 @@ for index in range(Num_sawtooths_2):
 
 with open("Testing_Cutoffs.txt",'w') as f: 
     for line in Full_Data_Set:
-        f.write(str(line).strip("([])")+'\n') #Writing each line in that file
-    f.close() #Closing the file to save it
+        f.write(str(line).strip("([])")+'\n')
+    f.close() #Close file to make sure it saves!
     
-    Full_Data_Array = np.array(Full_Data_Set) #Converting from a list to an array
+Full_Data_Array = np.array(Full_Data_Set) #Put data analysis after this line!
